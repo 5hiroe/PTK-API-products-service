541561361f707b327b2b1373f6ab687d82eb2f08
L-Durand <loic.durand.perso@gmail.com>
2024-09-10 22:46:38 +0200

    Add routes, controllers, services and validators

---
+++ src/validators/model.js
@@ -0,0 +1,8 @@
+import Joi from 'joi'
+
+export const product = Joi.object({
+    name: Joi.string().max(100).required(),
+    description: Joi.string().max(100).required(),
+    price: Joi.string().max(10).required(),
+    stock_quantity: Joi.string().max(10).required(),
+})
