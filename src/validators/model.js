453e2eee9660b7eb18e7641ad832db38bebb318b
KillianMalon <killian.malon@epsi.fr>
2024-09-18 22:28:26 +0200

    Resolve PR conflict

--- src/validators/model.js
+++ src/validators/model.js
@@ -4,5 +4,5 @@ export const product = Joi.object({
     name: Joi.string().max(100).required(),
     description: Joi.string().max(100).required(),
     price: Joi.string().max(10).required(),
-    stockQuantity: Joi.string().max(10).required(),
+    stockQuantity: Joi.number().required(),
 })
